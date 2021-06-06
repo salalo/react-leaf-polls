32a3a1bfdfa758492b5635f6bf2749185236626c
WojciechSala <wojteksala190@gmail.com>
2021-06-07 01:09:44 +0200

    testing plan

---
+++ src/tests/BinaryPoll.test.tsx
@@ -0,0 +1,15 @@
+describe('binary poll specs', () => {
+  // countPercentage()
+  // % key exists in the object
+  // % key stores the right value?
+  it('', () => {})
+  // animateAnswers()
+  // % value renders and is equal to the object value
+  // background color of clicked answer changes
+  // width is equal to % value
+  // height does not change
+  it('', () => {})
+  // Component
+  // onClick executes the commands and modifies state
+  // question renders when valid prop is passed
+})
