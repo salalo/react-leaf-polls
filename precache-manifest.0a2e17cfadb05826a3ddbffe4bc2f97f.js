b0dace2d9625965e6e3f29de121dc1bea1164376
salalo <wojteksala190@gmail.com>
2022-04-29 19:08:22 +0200

    Updates

---
+++ precache-manifest.0a2e17cfadb05826a3ddbffe4bc2f97f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b4dde023879ecaaa0024113239b519",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2373f5e7cff14c8794b2",
+    "url": "./static/css/main.f9117cc3.chunk.css"
+  },
+  {
+    "revision": "badda7a07e0314bc5cef",
+    "url": "./static/js/2.5bda2bb7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5bda2bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2373f5e7cff14c8794b2",
+    "url": "./static/js/main.36200175.chunk.js"
+  },
+  {
+    "revision": "e2930c3443e8394e9338",
+    "url": "./static/js/runtime-main.30eac3a1.js"
+  }
+]);
